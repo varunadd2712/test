3ca64c860d137d1751eefbfb5a10429cd357f1e9
Sai Varun Addanki <saivarun.addanki@utah.edu>
2019-04-09 18:36:59 -0600

    Adding brush move and reset.

--- src/helper.ts
+++ src/helper.ts
@@ -1,16 +1,23 @@
 import * as d3 from 'd3'
 
+
+var g;
+var gBrush;
+var brush;
+
 export interface stateObject {
     order : number[];
 }
 
+export interface stateObject2 {
+    xValue : number;
+}
+
 export function resetState(prevState : stateObject) {
-  console.log("touchdown");
-  console.log("called with");
-  console.log(prevState);
+  //console.log(prevState);
   let listsOfKey = Object.keys(prevState)[0];
   let dataFile = document.getElementById('aBarChart').children;
-  console.log(dataFile);
+  //console.log(dataFile);
   let dataFileCopy = [];
 
   for(let j = 0 ; j < dataFile.length ; j++) {
@@ -20,6 +27,21 @@ export function resetState(prevState : stateObject) {
   for(let j = 0 ; j < dataFile.length ; j++) {
     dataFile[j].setAttribute("width", dataFileCopy[j]);
   }
+
+  return prevState;
+}
+
+export function resetState2(prevState : stateObject2) {
+
+  let brushG = d3.select(".selection");
+  console.log("logging in reset")
+  let keyValue = Object.keys(prevState)[0];
+  console.log(prevState[keyValue].xValue);
+  let currentWidth = +brushG.attr("width");
+
+  console.log(currentWidth);
+
+  gBrush.call(brush.move, [prevState[keyValue].xValue, prevState[keyValue].xValue + currentWidth]);
 
   return prevState;
 }
@@ -50,6 +72,23 @@ export async function changeData() {
   }
 }
 
+var globalMax;
+
+function calculateGlobalMax(data) {
+  globalMax = 0;
+  let rowMax;
+  for(let i = 0 ; i < data.length ; i++) {
+
+    if(+data[i].a > +data[i].b)
+    rowMax = +data[i].a;
+    else
+    rowMax = +data[i].b;
+
+    if(rowMax > globalMax)
+    globalMax = rowMax;
+  }
+}
+
 export function staircase() {
   let dataFile = document.getElementById('aBarChart').children;
   let arr = [];
@@ -69,8 +108,8 @@ export function staircase() {
     stateArray.push(arr[j][1]);
   }
   const currentState : stateObject = ({order : stateArray});
-  console.log("Returning the value as");
-  console.log(currentState);
+  //console.log("Returning the value as");
+  //console.log(currentState);
   return currentState;
 
 }
@@ -204,9 +243,92 @@ export function update(data) {
   let selectionAreaB = d3.selectAll("#bAreaChart");
   selectionAreaB.attr("d", bAreaGenerator(data));
 
+
+  let selectionScatter = d3.selectAll("#scatterplot");
+  console.log("selected");
+  console.log(selectionScatter);
+  console.log("before removal");
+  console.log(selectionScatter.selectAll("circle"));
+  selectionScatter.selectAll("circle").remove();
+
+  let selectionCircles = selectionScatter.selectAll("circle");
+  console.log("after removal");
+  console.log(selectionCircles);
+
+  let dataselectionScatter = selectionCircles.data(data);
+
+  function brushed(){
+         //let [x1,y1] = d3.event.selection[0]
+         //let [x2,y2] = d3.event.selection[1]
+
+         //console.log(x1)
+         //console.log(y1)
+       }
+
+  let scatterplotSvg = d3.select("#scatterplotSvg");
+  let brush = d3.brush().extent([[0, 0],[160, 160]]).on("brush end", brushed);
+  scatterplotSvg.append("g").attr("class", "brush").call(brush);
+  let brushVal = d3.select(".brush")
+  //d3.select(".brush").transition().call(brush.move, x.range());
+  brushVal.call(brush.move, [0, 160], [0, 160]);
+
+
+  calculateGlobalMax(data);
+
+  let totalScale = d3.scaleLinear()
+    .domain([0, globalMax])
+    .range([0, 140]);
+
+  dataselectionScatter
+    .exit()
+      .style("opacity", 1)
+    .transition()
+    .duration(3000)
+      .style("opacity", 0)
+    .remove();
+
+  let newScatter = dataselectionScatter
+                    .enter()
+                    .append("circle")
+                      .attr("cx", function(d:any, i) {
+                        return totalScale(d.a);
+                      })
+                      .attr("cy", function(d:any, i) {
+                        return totalScale(d.b);
+                      })
+                      .attr("r", 5)
+                      .style("opacity", 0);
+
+  newScatter
+    .transition()
+    .duration(3000)
+      .attr("cx", function(d:any, i) {
+        return totalScale(d.a);
+      })
+      .attr("cy", function(d:any, i) {
+        return totalScale(d.b);
+      })
+      .attr("r", 5)
+      .style("opacity", 1);
+
+  //Assigning click function using d3.
+
+  //Assigning tooltip for the scatter points.
+  dataselectionScatter.selectAll("title").remove();
+
+  dataselectionScatter
+    .append("title")
+    .text(function(d:any, i) {
+      return "X is " + totalScale(d.a) + " and Y is " + totalScale(d.b)
+    });
+
+  //Interaction on bar chart are assigned below.
+
   const currentStateVal : stateObject = ({order : currentState});
-  console.log("Returning the value as");
-  console.log(currentStateVal);
+
+
+  //console.log("Returning the value as");
+  //console.log(currentStateVal);
   return currentStateVal;
 }
 
@@ -214,4 +336,72 @@ export function returnFreshState() {
   let currentState = [0,1,2,3,4,5,6,7,8,9,10];
   let currentStateVal : stateObject = ({order : currentState});
   return currentStateVal;
+}
+
+
+export function scatterPlotFunction() {
+
+  let data = d3.range(800).map(Math.random);
+
+  let svg = d3.select("#testDiv").select("svg");
+  let margin = {top: 194, right: 50, bottom: 214, left: 50};
+  let width = +svg.attr("width") - margin.left - margin.right;
+  let height = +svg.attr("height") - margin.top - margin.bottom;
+  g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  let x = d3.scaleLinear().range([0, width]),
+      y = d3.randomNormal(height / 2, height / 8);
+
+  brush = d3.brushX()
+      .extent([[0, 0], [width, height]])
+      .on("start brush end", brushmoved);
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  let circle = g.append("g")
+      .attr("class", "circle")
+    .selectAll("circle")
+    .data(data)
+    .enter().append("circle")
+      .attr("transform", function(d) { return "translate(" + x(d) + "," + y() + ")"; })
+      .attr("r", 3.5);
+
+  gBrush = g.append("g")
+      .attr("class", "brush")
+      .call(brush);
+
+  let handle = gBrush.selectAll(".handle--custom")
+    .data([{type: "w"}, {type: "e"}]);
+
+  gBrush.call(brush.move, [500, 700]);
+
+  function brushmoved() {
+    var s = d3.event.selection;
+    console.log(s)
+    if (s == null) {
+      handle.attr("display", "none");
+      circle.classed("active", false);
+    } else {
+      var sx = s.map(x.invert);
+      circle.classed("active", function(d) { return sx[0] <= d && d <= sx[1]; });
+      handle.attr("display", null).attr("transform", function(d, i) { return "translate(" + s[i] + "," + height / 2 + ")"; });
+    }
+  }
+}
+
+export function moveBrushNow() {
+  console.log("Call reached here");
+  let brushG = d3.select(".selection");
+  let currentx = +brushG.attr("x");
+  let currentWidth = +brushG.attr("width");
+
+  gBrush.call(brush.move, [currentx + 10, currentx + 10 + currentWidth]);
+  let currentStateVal : stateObject2 = ({xValue : currentx + 10});
+
+  return currentStateVal;
+
+  //console.log(brush.getattr("x"));
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from "redux";
 import { Provenance } from "provenance-lib-core/lib/src";
-import {resetState, changeData, staircase, returnFreshState, stateObject} from "./helper";
+import {resetState, changeData, staircase, returnFreshState, stateObject, scatterPlotFunction, moveBrushNow, stateObject2, resetState2} from "./helper";
 
 import {
   ReversibleAction,
@@ -13,10 +13,30 @@ export enum VizActionsEnum {
   RESET = "RESET"
 }
 
+scatterPlotFunction();
+console.log("start test");
+//moveBrushNow();
+
+
+export enum Viz2ActionsEnum {
+  MOVE_BRUSH = "MOVE_BRUSH",
+  RESET_BRUSH = "RESET_BRUSH"
+}
+
 interface VizAction {
   type: VizActionsEnum;
   args: stateObject;
 }
+
+interface Viz2Action {
+  type: Viz2ActionsEnum;
+  args: stateObject2;
+}
+
+export const createBrushAction = (toSet:stateObject2): Viz2Action => ({
+  type: Viz2ActionsEnum.MOVE_BRUSH,
+  args: toSet
+});
 
 export const createStaircaseAction = (toSet:stateObject): VizAction => ({
   type: VizActionsEnum.STAIRCASE,
@@ -32,6 +52,26 @@ export const createResetAction = (toSet:stateObject): VizAction => ({
   type: VizActionsEnum.DATASET_UPDATE,
   args: toSet
 });
+
+const viz2Reducer = (count: stateObject2, action: Viz2Action) => {
+  switch (action.type) {
+    case "DO_" + Viz2ActionsEnum.MOVE_BRUSH: {
+      console.log("In this block");
+      return moveBrushNow();
+    }
+
+    case Viz2ActionsEnum.RESET_BRUSH: {
+      //console.log("recieved args", action.args);
+      resetState2(action.args);
+      return action.args;
+    }
+
+    default: {
+      console.log("In this guy")
+        return moveBrushNow();
+    }
+  }
+};
 
 const vizReducer = (count: stateObject, action: VizAction) => {
   switch (action.type) {
@@ -52,7 +92,7 @@ const vizReducer = (count: stateObject, action: VizAction) => {
     }
 
     case VizActionsEnum.RESET: {
-      console.log("recieved args", action.args);
+      //console.log("recieved args", action.args);
       resetState(action.args);
       return action.args;
     }
@@ -63,6 +103,13 @@ const vizReducer = (count: stateObject, action: VizAction) => {
   }
 };
 
+export const Vizualization2 = () =>
+  createStore(
+    combineReducers({
+      count: viz2Reducer
+    })
+  );
+
 export const Vizualization1 = () =>
   createStore(
     combineReducers({
@@ -71,8 +118,14 @@ export const Vizualization1 = () =>
   );
 
 const app = Vizualization1();
+const app2 = Vizualization2();
 
+const provenance2 = Provenance(app2, "RESET_BRUSH");
 const provenance = Provenance(app, "RESET");
+
+const createReversibleMoveBrushAction = (toSet: number): ReversibleAction<number, number> => {
+  return ReversibleActionCreator(Viz2ActionsEnum.MOVE_BRUSH, toSet, toSet);
+};
 
 const createReversibleStaircaseAction = (toSet: number): ReversibleAction<number, number> => {
   return ReversibleActionCreator(VizActionsEnum.STAIRCASE, toSet, toSet);
@@ -94,12 +147,31 @@ button2.addEventListener("click", (e2:Event) => doStaircase());
 const button4:HTMLElement = document.getElementById('undo-staircase');
 button4.addEventListener("click", (e2:Event) => callUndo());
 
+const button5:HTMLElement = document.getElementById('move brush');
+button5.addEventListener("click", (e2:Event) => moveBrushNowHere());
+
+const button6:HTMLElement = document.getElementById('reset brush');
+button6.addEventListener("click", (e2:Event) => undoBrush());
+
+
 const act = createReversibleStaircaseAction(3);
 const act2 = createReversibleUpdateAction(3);
 
+const act3 = createReversibleMoveBrushAction(3);
+
+function moveBrushNowHere() {
+    console.log("At least reached");
+    provenance2.apply(act3);
+    //console.log(provenance.graph());
+}
+
 function doStaircase() {
   provenance.apply(act);
-  console.log(provenance.graph());
+  //console.log(provenance.graph());
+}
+function undoBrush() {
+  //provenance.goBackNSteps(1);
+  provenance2.goBackOneStepWithState();
 }
 
 function callUndo() {
@@ -109,4 +181,5 @@ function callUndo() {
 
 function doUpdate() {
   provenance.apply(act2);
+  //console.log(provenance.graph());
 }
